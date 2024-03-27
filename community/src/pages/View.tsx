3589215b4768baf067c7f22545bdf0a30d882c91
Yurisn1 <dev.yurisn@gmail.com>
2024-03-27 03:20:30 -0300

    ♻️ Refactor View component and add NavBar

--- community/src/pages/View.tsx
+++ community/src/pages/View.tsx
@@ -1,37 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import FrameViewerComponent from '../components/FrameViewerComponent';
-
-interface Frame {
-  id: number;
-  imageUrl: string;
-}
+import NavBar from '../components/navbar';
+import './../styles/view.css';
 
 const View: React.FC = () => {
   const { videoId } = useParams<{ videoId: string }>();
-  const [frames, setFrames] = useState<Frame[]>([]);
 
-  useEffect(() => {
-    const fetchFrames = async () => {
-      try {
-        const response = await fetch(`/api/videos/${videoId}/frames`);
-        if (!response.ok) {
-          throw new Error('Failed to fetch frames.');
-        }
-        const data = await response.json();
-        setFrames(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchFrames();
-  }, [videoId]);
+  // Provide a default value for videoId using the nullish coalescing operator
+  const id = videoId ?? '';
 
   return (
     <div>
-      <h1>Visualização de Frames</h1>
-      <FrameViewerComponent frames={frames} />
+      <NavBar />
+      <FrameViewerComponent videoId={id} />
     </div>
   );
 };
