5df3e6e106b488f0dbf29d7aaf83adb3e5ed7d35
Yurisn1 <dev.yurisn@gmail.com>
2024-03-27 03:15:28 -0300

    🔥 Remove unused routes

--- server/src/routes/listRoutes.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/listRoutes.ts
-import express from 'express';
-import { listVideos } from '../controllers/listController';
-
-const router = express.Router();
-
-router.get('/list', listVideos);
-
-export default router;
--- server/src/routes/uploadRoutes.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/uploadRoutes.ts
-import express from 'express';
-import { uploadVideo } from '../controllers/uploadController';
-
-const router = express.Router();
-
-router.post('/upload', uploadVideo);
-
-export default router;
