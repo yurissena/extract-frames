b7fc441377219b53f7f50408b089ec0ed232c894
Yurisn1 <dev.yurisn@gmail.com>
2024-03-27 05:56:41 -0300

    🎨 Update logo import paths

--- community/src/components/navbar.tsx
+++ community/src/components/navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/navbar.css';
-import logo from '../logo.svg';
+import logo from './../assets/logo.svg';
 
 const NavBar: React.FC = () => {
 
