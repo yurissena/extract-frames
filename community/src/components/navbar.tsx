c3414fd08a89edf919a8f17c54ad15815b92fbc6
Yurisn1 <dev.yurisn@gmail.com>
2024-03-27 04:07:57 -0300

    🎨 Update navbar width in CSS

--- community/src/components/navbar.tsx
+++ community/src/components/navbar.tsx
@@ -14,7 +14,6 @@ const NavBar: React.FC = () => {
         </Link>
       </div>
       <ul>
-        <li><Link to="/" aria-label="Página inicial">Home</Link></li>
         <li><Link to="/upload" aria-label="Página de Upload">Upload</Link></li>
         <li><Link to="/list" aria-label="Lista de Vídeos">Lista de Vídeos</Link></li>
       </ul>
